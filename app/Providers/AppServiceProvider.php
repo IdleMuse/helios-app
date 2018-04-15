4909d03710d87dc036f91d5c1df704814444604a
Justin Dee <idlemuse@ghostlit.co.uk>
2018-04-15 21:01:26 +0100

    MorphMap for handling asset subtypes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,26 +3,17 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Relations\Relation;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
+    public function boot(){
+        Relation::morphMap([
+            'ship' => 'App\Ship',
+        ]);
     }
 
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
+    public function register(){
         //
     }
 }
